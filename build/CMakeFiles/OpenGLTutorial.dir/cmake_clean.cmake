90660b8a46c95aed51f38ff6f2c4bb184ade7961
Roger Massana <rogermassana2002@gmail.com>
2024-05-31 13:22:12 +0200

    Imported dependencies

---
+++ build/CMakeFiles/OpenGLTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTutorial.dir/src/Shader.cpp.o"
+  "CMakeFiles/OpenGLTutorial.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/OpenGLTutorial.dir/src/main.cpp.o"
+  "CMakeFiles/OpenGLTutorial.dir/src/main.cpp.o.d"
+  "CMakeFiles/OpenGLTutorial.dir/src/stb_image.cpp.o"
+  "CMakeFiles/OpenGLTutorial.dir/src/stb_image.cpp.o.d"
+  "OpenGLTutorial"
+  "OpenGLTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OpenGLTutorial.dir/src/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/OpenGLTutorial.dir/src/main.cpp.o: \
+ /home/roger/Documents/programming/maze-opengl/src/main.cpp \
+ /usr/include/stdc-predef.h
